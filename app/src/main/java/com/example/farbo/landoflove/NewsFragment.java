61cc0cdd116600a9a702b42561d7d7ce9ed7b689
farbodassl <32558016+farbodassl@users.noreply.github.com>
2018-04-24 15:59:28 +0200

    Initial commit

---
+++ app/src/main/java/com/example/farbo/landoflove/NewsFragment.java
@@ -0,0 +1,19 @@
+package com.example.farbo.landoflove;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NewsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view;
+        view = inflater.inflate(R.layout.news_layout, container , false);
+
+        return view;
+    }
+}
